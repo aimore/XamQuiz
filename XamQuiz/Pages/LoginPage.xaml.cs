c877bc424a89f12e27c67e53b632deeeea43a812
Aimore Sa <aimoresc@gmail.com>
2019-06-11 22:15:52 +1000

    UI improvments, animations added 💅
    Register animation added
    Forgot password popup view created
    UI improvements

--- XamQuiz/Pages/LoginPage.xaml.cs
+++ XamQuiz/Pages/LoginPage.xaml.cs
@@ -12,12 +12,44 @@ namespace XamQuiz.Pages
     public partial class LoginPage : ContentPage
     {
         ViewModels.LoginPageViewModel vm;
+
         public LoginPage()
         {
             InitializeComponent();
             if (BindingContext == null)
                 BindingContext = vm = new ViewModels.LoginPageViewModel();
             NavigationPage.SetHasNavigationBar(this, false);
+            foreach (var child in container.Children)
+                child.Scale = 0;
+            popup.Scale = 0;
+        }
+
+        bool _isRegistering;
+
+        async void Register(object sender, System.EventArgs e)
+        {
+            _isRegistering = !_isRegistering;
+            btn.Text = _isRegistering ? "Register" : "Login";
+            if (_isRegistering)
+                // scale in the children for the panel
+                foreach (var child in container.Children)
+                {
+                    await child.ScaleTo(1.2, 100, Easing.CubicIn);
+                    await child.ScaleTo(1, 100, Easing.CubicOut);
+                }
+            else
+                //hide
+                foreach (var child in container.Children)
+                {
+                    await child.ScaleTo(1.2, 100, Easing.CubicIn);
+                    await child.ScaleTo(0, 100, Easing.CubicOut);
+                }
+        }
+
+        async void ShowPopup(object sender, System.EventArgs e)
+        {
+            await popup.ScaleTo(1.2, 100, Easing.BounceOut);
+            await popup.ScaleTo(1, 100, Easing.BounceIn);
         }
     }
 }
---
+++ XamQuiz/Pages/ResetPasswordPopup.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamQuiz.Pages
+{
+    public partial class ResetPasswordPopup : Frame
+    {
+        public ResetPasswordPopup()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            titleLbl.Text = "Password reseted!";
+            await Task.Delay(1000);
+            await this.ScaleTo(1.2, 100, Easing.BounceIn);
+            await this.ScaleTo(0, 100, Easing.BounceOut);
+            titleLbl.Text = "Reset password";
+
+        }
+    }
+}
